470e78b4c6cbd1dc0b516a4ab54396f8570ceaa9
suazzang <suaaa77777@gmail.com>
2024-05-13 17:51:39 +0900

    Updates

---
+++ precache-manifest.c4e13dfd22a60a4553fac28ab64ea7ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bda0e3c0b996fc30da779b63b3bfd8",
+    "url": "/shinee-fansite/index.html"
+  },
+  {
+    "revision": "3096f2213a48e0f995aa",
+    "url": "/shinee-fansite/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "51c56d999af033147613",
+    "url": "/shinee-fansite/static/css/main.14a3895e.chunk.css"
+  },
+  {
+    "revision": "3096f2213a48e0f995aa",
+    "url": "/shinee-fansite/static/js/2.f9f85bee.chunk.js"
+  },
+  {
+    "revision": "9f0cc9b61f48b176c9bd0ade4e69c9e0",
+    "url": "/shinee-fansite/static/js/2.f9f85bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7089efbddf9afec3b5f0",
+    "url": "/shinee-fansite/static/js/3.866922ec.chunk.js"
+  },
+  {
+    "revision": "51c56d999af033147613",
+    "url": "/shinee-fansite/static/js/main.d2db2db9.chunk.js"
+  },
+  {
+    "revision": "19484c33cbabfc1923bd",
+    "url": "/shinee-fansite/static/js/runtime-main.118bbafb.js"
+  }
+]);
